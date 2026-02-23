05afe045c9a875271f8bee6dc3368633d65d3d1f
axelfrache <axel.frache@etu.umontpellier.fr>
2026-02-23 12:22:38 +0100

    feat: initialize Spring Boot backend with Maven and Docker

---
+++ backend/src/main/java/com/axelfrache/signalbroker/SignalBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.axelfrache.signalbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalBrokerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/axelfrache/signalbroker/SignalBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axelfrache.signalbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
